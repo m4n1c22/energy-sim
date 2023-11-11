cddda359c8799ab395f8124fcd85ad04374a1d63
ram <sreeram.sadasivam@egym.com>
2023-11-11 21:33:46 +0100

    Consumer level sim done.

--- src/frontend/gui.py
+++ src/frontend/gui.py
@@ -3,6 +3,35 @@ import csv
 
 app = Flask(__name__)
 
+energy_usage_per_hr = {
+	"ref": 167,
+	"tv": 50,
+	"micro":1000,
+	"wm":850,
+	"dish":1800,
+	"comp": 200
+}
+
+savings = 0.0
+
+def calculate_energy_usage(ref, tv, micro, wm, dish, comp):
+	usage = 0
+	if ref=="true":
+		usage += energy_usage_per_hr["ref"]
+	if tv=="true":
+		usage += energy_usage_per_hr["tv"]
+	if micro=="true":
+		usage += energy_usage_per_hr["micro"]
+	if wm=="true":
+		usage += energy_usage_per_hr["wm"]
+	if dish=="true":
+		usage += energy_usage_per_hr["dish"]
+	if comp=="true":
+		usage += energy_usage_per_hr["comp"]
+	if usage == 0:
+		usage = 8
+	return usage
+
 
 @app.route('/')
 def index():
@@ -13,6 +42,9 @@ def index():
 @app.route('/plan/<schedulenum>')
 def plan(schedulenum):
 	schedule_num = int(schedulenum)
+	global savings
+	scheduled_usage = 0
+	unscheduled_usage = 0
 	ref, tv, micro, wm, dish, comp, iref, itv, imicro, iwm, idish, icomp = None, None, None, None, None, None, None, None, None, None, None, None
 	with open('data/unscheduled.csv', newline='') as csv_file:
 		csv_reader = csv.reader(csv_file, delimiter=',')
@@ -25,8 +57,10 @@ def plan(schedulenum):
 				wm 		= "true" if row[5] == "1" else "false"
 				dish 	= "true" if row[6] == "1" else "false"
 				comp 	= "true" if row[7] == "1" else "false"
+				unscheduled_usage = calculate_energy_usage(ref, tv, micro, wm, dish, comp)
 				break
 			linecount += 1
+	
 
 	with open('data/scheduled.csv', newline='') as csv_file:
 		csv_reader = csv.reader(csv_file, delimiter=',')
@@ -39,11 +73,16 @@ def plan(schedulenum):
 				iwm 	= "true" if row[5] == "1" else "false"
 				idish 	= "true" if row[6] == "1" else "false"
 				icomp 	= "true" if row[7] == "1" else "false"
+				scheduled_usage = calculate_energy_usage(iref, itv, imicro, iwm, idish, icomp)
 				break
 			linecount += 1
 
+	int_savings = (unscheduled_usage - scheduled_usage)*100/unscheduled_usage 
 
-	return render_template("plan.html", ref= ref, tv= tv, micro=micro, wm=wm, dish=dish, comp=comp, iref=iref, itv=itv, imicro=imicro, iwm=iwm, idish=idish, icomp=icomp, schedulenum=schedule_num+1)
+	savings += int_savings
+
+
+	return render_template("plan.html", ref= ref, tv= tv, micro=micro, wm=wm, dish=dish, comp=comp, iref=iref, itv=itv, imicro=imicro, iwm=iwm, idish=idish, icomp=icomp, schedulenum=schedule_num+1, int_savings = str(round(int_savings, 5)),savings=str(round(savings, 5)))
 	
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=1234, threaded=True)
